c36c567df3ab6d2529bc07a638d5cf1ead658553
ArshKumar84 <105591962+ArshKumar84@users.noreply.github.com>
2024-10-07 16:35:38 +0530

    Update app-news-page.jsx

--- Front-end/src/components/app-news-page.jsx
+++ Front-end/src/components/app-news-page.jsx
@@ -14,7 +14,7 @@ const NewsPage = () => {
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const response = await fetch(`https://gnews.io/api/v4/search?q=environment&token=${YOUR_API_KEY}&lang=en`);
+        const response = await fetch(`https://gnews.io/api/v4/search?q=aqi&token=${YOUR_API_KEY}&lang=en`);
         
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
