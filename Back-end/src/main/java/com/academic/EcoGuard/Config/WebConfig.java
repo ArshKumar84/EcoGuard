875563eb465bfbe22825aee89f00041e461242dd
ArshKumar84 <105591962+ArshKumar84@users.noreply.github.com>
2024-10-09 22:47:52 +0530

    Update WebConfig.java
    updated allowedOrigins to front-end  correct url

--- Back-end/src/main/java/com/academic/EcoGuard/Config/WebConfig.java
+++ Back-end/src/main/java/com/academic/EcoGuard/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("https://eco-guard-rjq29kcqh-arshkumar84s-projects.vercel.app")
+                .allowedOrigins("https://eco-guard-ten.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
